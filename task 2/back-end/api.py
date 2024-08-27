3d8bf77cc2f34bd4a018979aba9047b069a7e50b
francois0002 <francois.gosselin.lille@gmail.com>
2024-08-27 15:11:02 +0200

    update index htm to close H1 tag

--- task 2/back-end/api.py
+++ task 2/back-end/api.py
@@ -1,8 +1,10 @@
 """ Make an API that returns a hello world. """
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/hello')
 def hello_world():
